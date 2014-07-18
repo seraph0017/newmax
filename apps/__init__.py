3c11cf6e3ceebc02ed37116e04e44dea5a17ff5a
max <max@zamplus.com>
2014-07-18 15:08:05 +0800

    update

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python
 #encoding:utf-8
+import yaml
+import os
+
 from flask import Flask
 from ext import db, mail, cache
 
+
+with open('config.yml','rb') as f:
+    config_file = f.read()
+
+conf = yaml.load(config_file)
+
 app = Flask(__name__)
+app.config.update(conf['ENV'][os.getenv('ENV')])
+
+
+
+
 db.init_app(app)
 mail.init_app(app)
-cache.init_app(app)
+cache.init_app(app,config=conf['CACHE_CONFIG'])
